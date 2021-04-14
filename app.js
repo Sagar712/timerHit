56d117687519e7ed5d22cf8e51288e3b25292ec2
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-04-14 16:31:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,70 @@
-let timesh = document.getElementById("timeShow");
+
+const AppUrl = 'https://secret-script.herokuapp.com/script/';
+let dateIS = new Date();
+let hours = dateIS.getHours();
+let mins = dateIS.getMinutes();
+let poster = "am"
+if(hours){
+    poster = "pm"
+    if(hours>12){
+        hours = hours-12;
+    }
+} 
+
+
 window.setInterval(function(){
     let dateIS = new Date();
     console.log(dateIS.getHours() +" "+dateIS.getMinutes()+" "+dateIS.getSeconds());
-    timesh.innerHTML = `${dateIS.getHours()} : ${dateIS.getMinutes()} : ${dateIS.getSeconds()}`;
-    if(dateIS.getHours() == 22 && dateIS.getMinutes() == 57){
-        console.log("Timer Hit!!");
+    
+    let hours = dateIS.getHours();
+    let mins = dateIS.getMinutes();
+    let secs = dateIS.getSeconds();
+    let poster = "am"
+    if(hours){
+        poster = "pm"
+        if(hours>12){
+            hours = hours-12;
+        }
+    } 
+    
+
+
+    let spin = document.getElementById("spineerTrial");
+    let spinHour = document.getElementById("spineerTrialHours");
+    let extra = parseInt((mins*6)/72);
+
+    console.log("extra degress of hour:" +((hours*30)+extra));
+    
+    
+    spinHour.style.transform = `rotate(${((hours*30)+extra)}deg)`;
+    spin.style.transform = `rotate(${mins*6}deg)`;
+
+    /*
+    if(dateIS.getHours() == 11 && dateIS.getMinutes() == 6){
+
+        console.log("Calling DB");
+        postdb()
+        
     }
+    */
+
 }, 1000);
 
+function postdb() {
+
+
+    let data = {
+        username: "Testing Timer app",
+        email: "BackgroundupportCheck",
+        password: "Success!!"
+    };
+
+    fetch(AppUrl,{method:'post', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
+        .then(res => {
+            return res.json();
+        })
+        .then(response => {
+            console.log(response);
+            alert(`${response.msg}`);
+        });
+}
